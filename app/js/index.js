936d83e5a6569b51f137ea8fbd8364fcf3aa40f4
ManojSilag <manojsilag@gmail.com>
2019-11-30 22:02:27 +0530

    Initialize the project

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+/* styling */
+require('styles/main.scss');
+/* js */
+import $ from 'jquery';
+import { log, logTitle } from 'logger';
+/* your imports */
+logTitle('Title');
+/* coding examples */
+log('Hi')
