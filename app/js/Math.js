64df42efcaf185db2d1d2874a0aaf59d36f636bb
ManojSilag <manojsilag@gmail.com>
2019-12-01 12:58:22 +0530

    update

---
+++ app/js/Math.js
@@ -0,0 +1,17 @@
+export var add = function(n1,n2){
+    return n1 + n2;
+}
+
+export var sub = function(n1,n2){
+    return n1 -  n2;
+}
+
+export var mul = function(n1,n2){
+    return n1 * n2;
+}
+
+export var div = function(n1,n2){
+    return n1 /  n2;
+}
+
+export var pi = 3.1415
