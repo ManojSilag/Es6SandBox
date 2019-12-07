44832948e3381536a702761e24226b6e3c6fc539
ManojSilag <manojsilag@gmail.com>
2019-12-07 21:19:12 +0530

    update

--- app/js/index.js
+++ app/js/index.js
@@ -3,37 +3,72 @@ require('styles/main.scss');
 /* js */
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Es6 Classes');
 
-// const apicall = new Promise((resolve, reject)=>{
+import { coroutine as co } from 'bluebird';
 
-//   setTimeout(()=>{
-//      resolve('True')
-//   },2000)
+logTitle('Generators');
 
-//   setTimeout(()=>{
-//     reject('True')
-//  },3000)
+// const getValues = function* (){
+//   yield 1;
+//   yield "hello";
+//   yield true;
+//   yield { name: 'Alex'}
+//   return 'I am done';
+// }
 
-// })
+// const numberGen = getValues();
 
-// apicall.then(result => {
-//   log(result);
-// })
+// log(numberGen.next().value)
+// log(numberGen.next().value)
+// log(numberGen.next().value)
+// log(numberGen.next().value)
+// log(numberGen.next().value)
 
-const RandomUsers = n => {
-  const fetchUsers = fetch(`https://randomuser.me/api/?results=${n}`)
-  fetchUsers.then(data => {
-    data.json().then(data => {
-      // log(JSON.stringify(data.results.length))
-      JSON.stringify(data.results.length)
-      data.results.forEach(element => {
-      const{ email} = element;
-       log(email);
-    });
-    
-    })
-  })
-}
 
-RandomUsers(50);
+// const getNumbers = function* (numbers){
+//   for (let index = 0; index < numbers.length; index++) {
+//     yield numbers[index]
+//   }
+// }
+
+// const getNumbersGen = getNumbers([1,22,33,44,55]);
+
+// const interval = setInterval(() => {
+//   const next = getNumbersGen.next();
+//   if(next.done){
+//     log('This Gen is done');
+//     clearInterval(interval);
+//   }else{
+//     const number = next.value;
+//     log(number);
+//   }
+// }, 1000)
+
+
+// const getRandomUsers = n => {
+//   const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`)
+//   fetchRandomUsers.then(data => {
+//     data.json().then(randomUsers => {
+//       log(JSON.stringify(randomUsers.results.length));
+//       randomUsers.results.forEach(user => {
+//         const {gender, email} = user;
+//         log(`${gender} - ${email}`);
+//       });
+//     })
+//   });
+// }
+
+// getRandomUsers(100);
+
+const getRandomUsers = co(function* (n){
+  const fetchRandomUsers = yield fetch(`https://randomuser.me/api/?results=${n}`)
+  const data = yield fetchRandomUsers.json();
+  return data;
+});
+
+getRandomUsers(10).then(randomUsers => {
+        randomUsers.results.forEach(user => {
+        const {gender, email} = user;
+        log(`${gender} - ${email}`);
+      });
+}).catch(err => log(err))
